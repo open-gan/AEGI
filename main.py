d3f45fd8c730ad65c4f53a322a1d35574816008f
open-gan <74525665+open-gan@users.noreply.github.com>
2020-11-24 15:25:42 +0800

    main

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from networks import *
 
 np.set_printoptions(suppress=True)
 torch.set_printoptions(sci_mode=False)
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def parse_arg():
@@ -31,18 +31,18 @@ def parse_arg():
 
     parser.add_argument('--dataroot', default="", type=str, help='data path')
     parser.add_argument('--outfile', default='results', type=str, help='output path')
-    parser.add_argument('--noise_dim', type=int, default=512, help='latent space')
-    parser.add_argument('--batch_size', type=int, default=5, help='batch size')
-    parser.add_argument('--test_batch_size', type=int, default=5, help='test batch size')
+    parser.add_argument('--noise_dim', type=int, default=256, help='latent space')
+    parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+    parser.add_argument('--test_batch_size', type=int, default=32, help='test batch size')
     parser.add_argument('--nEpochs', type=int, default=1000, help='epochs')
     parser.add_argument('--sample_step', type=int, default=1000, help='iteration for saving image')
-    parser.add_argument('--save_step', type=int, default=2, help='epoch for saving mdoel')
+    parser.add_argument('--save_step', type=int, default=1, help='epoch for saving mdoel')
 
-    parser.add_argument('--channels', default="16, 32, 64, 128, 256, 512, 512, 512", type=str, help='channels for each layer')
+    parser.add_argument('--channels', default="32, 64, 128, 256, 512", type=str, help='channels for each layer')
     parser.add_argument('--trainsize', type=int, default=29000, help='training sample')
-    parser.add_argument('--input_height', type=int, default=1024, help='input image size')
+    parser.add_argument('--input_height', type=int, default=128, help='input image size')
     parser.add_argument('--input_width', type=int, default=None, help='input image size')
-    parser.add_argument('--output_height', type=int, default=1024, help='output image size')
+    parser.add_argument('--output_height', type=int, default=128, help='output image size')
     parser.add_argument('--output_width', type=int, default=None, help='output image size')
     parser.add_argument('--crop_height', type=int, default=None, help='crop image size')
     parser.add_argument('--crop_width', type=int, default=None, help='crop image size')
@@ -53,19 +53,18 @@ def parse_arg():
     parser.add_argument('--beta1', type=float, default=0.9, help='beta1')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2')
 
-    parser.add_argument('--m_plus', type=float, default=160.0, help='m in formula')
+    parser.add_argument('--m_plus', type=float, default=140.0, help='m in formula')
     parser.add_argument('--weight_neg', type=float, default=0.5, help='alpha * loss_adv， alpha in formula')
-    parser.add_argument('--weight_rec', type=float, default=0.0025, help='lambda * ae_loss， lambda in formula')
+    parser.add_argument('--weight_rec', type=float, default=0.2, help='lambda * ae_loss， lambda in formula')
     parser.add_argument('--weight_kl', type=float, default=1., help='gamma * kl(q||p)_loss，gamma in formula')
 
     # # parser.add_argument('--mean', type=float, default=[0.485, 0.456, 0.406], help='normalized mean')
     # # parser.add_argument('--std', type=float, default=[0.229, 0.224, 0.225], help='normalized var')
 
-    parser.add_argument('--use_tensorboard', type=bool, default=False)
     parser.add_argument("--pretrained", default="", type=str, help="path for pre-training model")
     parser.add_argument("--start_epoch", default=0, type=int, help="start epoch")
-    parser.add_argument("--num_vae", type=int, default=10, help="epochs for VAE pre-training")
-    parser.add_argument("--num_gan", type=int, default=10, help="epochs for GAN pre-training, without ExponentialMovingAverage")
+    parser.add_argument("--num_vae", type=int, default=0, help="epochs for VAE pre-training")
+    parser.add_argument("--num_gan", type=int, default=0, help="epochs for GAN pre-training, without ExponentialMovingAverage")
 
     return parser.parse_known_args()[0]
 
@@ -96,13 +95,13 @@ def main():
     str_to_list = lambda x: [int(xi) for xi in x.split(',')]
     model = IntroVAE(cdim=3, hdim=config.noise_dim,
                      channels=str_to_list(config.channels), image_size=config.output_height).to(device)
+    ema = EMA(model, 0.9999)
     if config.pretrained:
-        load_model(model, config.pretrained)
+        load_model(model, ema, config.pretrained)
     print(model)
 
     g_optimizer = torch.optim.Adam(model.generator.parameters(), config.g_lr, betas=(config.beta1, config.beta2))
     e_optimizer = torch.optim.Adam(model.encoder.parameters(), config.e_lr, betas=(config.beta1, config.beta2))
-    ema = EMA(0.999)
 
     # -----------------load dataset--------------------------
     image_list = [x for x in os.listdir(config.dataroot) if is_image_file(x)]
@@ -121,7 +120,7 @@ def main():
     fix_noise = torch.zeros(config.test_batch_size, config.noise_dim).normal_(0, 1).to(device)
 
     start_time = time.time()
-    cur_iter = 0
+    cur_iter = 72998
 
     def train_vae(epoch, iteration, real_images, cur_iter, statistic):
         info = "\n====> Cur_iter: [{}]: Epoch[{}]({}/{}): time: {:4.4f}: ".format(cur_iter, epoch, iteration,
@@ -162,6 +161,8 @@ def main():
             real_images = real_images.to(device)
 
             _, _, _, rec_images = model(real_images)
+            # mu, logvar = model.encode(real_images)
+            # rec_images = model.generator(mu)
 
             eps = [torch.cuda.FloatTensor(statistic['mu'].size()).normal_() for _ in range(config.test_batch_size)]
             eps = torch.stack(eps)
@@ -219,10 +220,8 @@ def main():
         lossG.backward()
         g_optimizer.step()
 
-        if epoch >= config.num_vae + config.num_gan:
-            for name, param in model.named_parameters():
-                if param.requires_grad:
-                    ema.update(name, param.data)
+        if epoch >= (config.num_vae + config.num_gan):
+            ema.update()
 
         info += 'Rec: {:.4f}, '.format(loss_rec.item())
         info += 'Kl_E: real {:.4f}, rec {:.4f}, '.format(lossE_real_kl.item(), _lossE_rec_kl.mean().item())
@@ -231,10 +230,15 @@ def main():
 
         if cur_iter % config.sample_step == 0:
             model.eval()
+            if epoch >= (config.num_vae + config.num_gan):
+                ema.apply_shadow()
+
             real_images = test_data_loader.next()
             real_images = real_images.to(device)
 
             _, _, _, rec_images = model(real_images)
+            # mu, logvar = model.encode(real_images)
+            # rec_images = model.generator(mu)
 
             eps = [torch.cuda.FloatTensor(statistic['mu'].size()).normal_() for _ in range(config.test_batch_size)]
             eps = torch.stack(eps)
@@ -244,6 +248,9 @@ def main():
 
             save_image(torch.cat([real_images, rec_images, fake_images, fix_fake_images], dim=0).data.cpu(),
                        '{}/image_{}.jpg'.format(config.outfile, cur_iter), nrow=config.test_batch_size // 2)
+
+            if epoch >= (config.num_vae + config.num_gan):
+                ema.restore()
             model.train()
 
     for epoch in range(config.start_epoch, config.nEpochs + 1):
@@ -257,19 +264,16 @@ def main():
             # --------------train------------
             if epoch < config.num_vae:
                 train_vae(epoch, iteration, real_images, cur_iter, statistic)
-            elif epoch == config.num_vae + config.num_gan:
-                for name, param in model.named_parameters():
-                    if param.requires_grad:
-                        ema.register(name, param.data)
-                train(epoch, iteration, real_images, cur_iter, statistic)
             else:
+                if epoch == (config.num_vae + config.num_gan):
+                    ema.register()
                 train(epoch, iteration, real_images, cur_iter, statistic)
 
             cur_iter += 1
 
         # save models
         if epoch % config.save_step == config.save_step - 1:
-            save_checkpoint(model, epoch, 0, statistic, '')
+            save_checkpoint(model, ema, epoch, 0, statistic, '')
 
 
 if __name__ == "__main__":
